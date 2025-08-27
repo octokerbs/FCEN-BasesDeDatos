09d4ebddde625e1e9a44595cc0ec926aa28dd433
octokerbs <kerbsod@gmail.com>
2025-08-27 17:57:37 -0300

    Levantar el CSV

--- Proyecto-BBDD/src/cli.ts
+++ Proyecto-BBDD/src/cli.ts
@@ -1,14 +1,26 @@
 import { Client } from 'pg';
-
 const client = new Client()
 await client.connect()
 
 import { readFile } from 'node:fs/promises';
 
-const filePath = `recursos/insert-ejemplo-alumnos.sql`;
+const filePath = `resources/alumnos.csv`;
 const contents = await readFile(filePath, { encoding: 'utf8' });
 
-const res = await client.query(contents)
+const header = contents.split(/\r?\n/)[0];
+const columns = header.split(',').map(col => col.trim());
 
-console.log(res.command, res.rowCount)
+const dataLines = contents.split(/\r?\n/).slice(1).filter(line => line.trim() !== '');
+
+for (const line of dataLines) {
+    const values = line.split(',');
+    const query = `
+        INSERT INTO aida.alumnos (${columns.join(', ')}) VALUES 
+            (${values.map((value) => value == '' ? 'null' : `'` + value + `'`).join(', ')})
+    `;
+    console.log(query)
+    const res = await client.query(query)
+    console.log(res.command, res.rowCount)
+}
+
 await client.end()
