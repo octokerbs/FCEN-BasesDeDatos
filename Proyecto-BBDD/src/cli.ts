b806a1bb8b76fc1b90898c1f06e7e2fc697e9403
octokerbs <kerbsod@gmail.com>
2025-08-27 18:25:59 -0300

    Delete previous data and load new one

--- Proyecto-BBDD/src/cli.ts
+++ Proyecto-BBDD/src/cli.ts
@@ -1,26 +1,38 @@
-import { Client } from 'pg';
-const client = new Client()
-await client.connect()
-
 import { readFile } from 'node:fs/promises';
+import { Client } from 'pg';
 
-const filePath = `resources/alumnos.csv`;
-const contents = await readFile(filePath, { encoding: 'utf8' });
+async function readAndParseCSV(filePath) {
+    const contents = await readFile(filePath, { encoding: 'utf8' });
+    const header = contents.split(/\r?\n/)[0];
+    const columns = header.split(',').map(col => col.trim());
+    const dataLines = contents.split(/\r?\n/).slice(1).filter(line => line.trim() !== '');
+    return { dataLines, columns };
 
-const header = contents.split(/\r?\n/)[0];
-const columns = header.split(',').map(col => col.trim());
+}
 
-const dataLines = contents.split(/\r?\n/).slice(1).filter(line => line.trim() !== '');
-
-for (const line of dataLines) {
-    const values = line.split(',');
-    const query = `
+async function refreshStudentTableFromCSV(pgClient, studentLinesArray, columns) {
+    await pgClient.query("DELETE FROM aida.alumnos");
+    for (const line of studentLinesArray) {
+        const values = line.split(',');
+        const query = `
         INSERT INTO aida.alumnos (${columns.join(', ')}) VALUES 
             (${values.map((value) => value == '' ? 'null' : `'` + value + `'`).join(', ')})
     `;
-    console.log(query)
-    const res = await client.query(query)
-    console.log(res.command, res.rowCount)
+        console.log(query)
+        const res = await pgClient.query(query)
+        console.log(res.command, res.rowCount)
+    }
 }
 
-await client.end()
+async function main() {
+    const pgClient = new Client()
+    const filePath = `resources/alumnos.csv`;
+
+    await pgClient.connect()
+
+    var { dataLines: studentLineArray, columns } = await readAndParseCSV(filePath)
+    await refreshStudentTableFromCSV(pgClient, studentLineArray, columns)
+    await pgClient.end()
+}
+
+main();
