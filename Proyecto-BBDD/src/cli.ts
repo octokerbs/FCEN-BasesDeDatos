005d3b41edc9f915f895673a24013a1fa20f64d2
octokerbs <kerbsod@gmail.com>
2025-08-31 18:10:29 -0300

    Al dia con typescript - Proximamente se migra a Go

--- Proyecto-BBDD/src/cli.ts
+++ Proyecto-BBDD/src/cli.ts
@@ -7,11 +7,6 @@ async function readAndParseCSV(filePath) {
     const columns = header.split(',').map(col => col.trim());
     const dataLines = contents.split(/\r?\n/).slice(1).filter(line => line.trim() !== '');
     return { dataLines, columns };
-
-}
-
-async function getFirstStudentThatNeedsCertificate(pgClient) {
-
 }
 
 async function refreshStudentTableFromCSV(pgClient, studentLinesArray, columns) {
@@ -28,14 +23,39 @@ async function refreshStudentTableFromCSV(pgClient, studentLinesArray, columns) 
     }
 }
 
+async function getFirstStudentThatNeedsCertificate(pgClient) {
+    const sql = `SELECT *
+    FROM aida.alumnos
+    WHERE titulo IS NOT NULL AND titulo_en_tramite IS NOT NULL
+    ORDER BY egreso
+	LIMIT 1`;
+    const res = await pgClient.query(sql)
+    if (res.rows.length > 0) {
+        return res.rows[0];
+    } else {
+        return null;
+    }
+}
+
+async function generateStudentCertificate(pgClient, student) {
+    console.log('student', student);
+}
+
 async function main() {
     const pgClient = new Client()
     const filePath = `resources/alumnos.csv`;
-
     await pgClient.connect()
 
     var { dataLines: studentLineArray, columns } = await readAndParseCSV(filePath)
     await refreshStudentTableFromCSV(pgClient, studentLineArray, columns)
+
+    var student = await getFirstStudentThatNeedsCertificate(pgClient)
+
+    if (student == null) {
+        console.log('No hay estudiantes que necesiten certificado')
+    } else {
+        await generateStudentCertificate(pgClient, student)
+    }
     await pgClient.end()
 }
 
